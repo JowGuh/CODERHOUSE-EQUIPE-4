c53be4411fa25090c0de851a8f790fb73cceac41
JowGuh <152320761+JowGuh@users.noreply.github.com>
2024-05-27 19:44:01 -0300

    Add files via upload

---
+++ projeto.py
@@ -0,0 +1,69 @@
+import sqlite3
+import requests
+
+
+api_url = "https://brasilapi.com.br/api/banks/v1"
+
+
+response = requests.get(api_url)
+data = response.json()
+
+
+conn = sqlite3.connect('bancos.db')
+cursor = conn.cursor()
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS bancos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    codigo TEXT,
+    ispb TEXT
+)
+''')
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS codigos_bancarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    banco_nome TEXT,
+    codigo_banco TEXT
+)
+''')
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS bancos_ispb (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    banco_nome TEXT,
+    ispb TEXT
+)
+''')
+
+
+for banco in data:
+    nome = banco.get('name', None)
+    codigo = banco.get('code', None)
+    ispb = banco.get('ispb', None)
+    if nome and codigo and ispb:
+        cursor.execute('''
+        INSERT INTO bancos (nome, codigo, ispb)
+        VALUES (?, ?, ?)
+        ''', (nome, codigo, ispb))
+        banco_id = cursor.lastrowid
+        
+        # Inserir dados na tabela de códigos bancários
+        cursor.execute('''
+        INSERT INTO codigos_bancarios (banco_nome, codigo_banco)
+        VALUES (?, ?)
+        ''', (nome, codigo))
+        
+        # Inserir dados na tabela de bancos por ISPB
+        cursor.execute('''
+        INSERT INTO bancos_ispb (banco_nome, ispb)
+        VALUES (?, ?)
+        ''', (nome, ispb))
+
+# Salvar (commit) as mudanças e fechar a conexão
+conn.commit()
+conn.close()
